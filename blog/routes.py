78f35378822dbe04b3ae04dce8e68aa856296994
Ianadika44 <adika19ian@gmail.com>
2020-05-13 21:35:10 +0300

    made route.py

--- blog/routes.py
+++ blog/routes.py
@@ -1,37 +1,37 @@
-import os
-from flask import render_template, url_for, flash, redirect,request,abort
-from blog import app,db,bcrypt
-from blog.models import User,Post
-from blog.forms import RegistrationForm, LoginForm,UpdateAccountForm,PostForm
-from flask_login import login_user,current_user,logout_user,login_required
-import secrets
+# import os
+# from flask import render_template, url_for, flash, redirect,request,abort
+# from blog import app,db,bcrypt
+# from blog.models import User,Post
+# from blog.forms import RegistrationForm, LoginForm,UpdateAccountForm,PostForm
+# from flask_login import login_user,current_user,logout_user,login_required
+# import secrets
 
 
-@app.route("/")
-@app.route("/home")
-def home():
-    posts = Post.query.all()
-    return render_template('home.html', posts=posts)
+# @app.route("/")
+# @app.route("/home")
+# def home():
+#     posts = Post.query.all()
+#     return render_template('home.html', posts=posts)
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
+# @app.route("/about")
+# def about():
+#     return render_template('about.html', title='About')
 
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email = form.email.data, password = hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been activated! You can now log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('home'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(username=form.username.data, email = form.email.data, password = hashed_password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Your account has been activated! You can now log in', 'success')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
